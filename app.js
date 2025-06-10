24047397e978051c96ccd5fcc689cbbcee22d8ef
Frankiga-GA <mguistv@gmail.com>
2025-06-10 10:44:38 -0500

    Lakguex2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import dashboardRoutes from "./src/modules/dashboard/routes/dashboard.route.js";
 
 // Cargar variables de entorno
 dotenv.config();
-console.log("Hola que hace")
+console.log("Hola que hace hchdfcfcfgcf")
 
 // Obtener __dirname en ES Modules
 const __filename = fileURLToPath(import.meta.url);
