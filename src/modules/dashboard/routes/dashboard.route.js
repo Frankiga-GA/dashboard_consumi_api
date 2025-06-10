4043d5bb2fe8b008759d57b37ca44a43303acea3
Frankiga-GA <mguistv@gmail.com>
2025-06-10 17:20:53 -0500

    Otro Api

--- src/modules/dashboard/routes/dashboard.route.js
+++ src/modules/dashboard/routes/dashboard.route.js
@@ -1,11 +1,8 @@
-// src/modules/dashboard/routes/dashboard.route.js
+import express from "express";
+import { getDashboard } from "../controllers/dashboard.controller.js";
 
-import express from 'express';
-import { getDashboard } from '../controllers/dashboard.controller.js';
+const router = express.Router();
 
-const dashboardRoutes = express.Router();
+router.get("/dashboard", getDashboard);
 
-// Ruta principal del dashboard
-dashboardRoutes.get('/dashboard', getDashboard);
-
-export default dashboardRoutes;
+export default router;
